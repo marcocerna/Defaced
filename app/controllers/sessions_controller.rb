67f0ddb7c9aa1cbafb266c96a8a2d6a4499de930
Marco <marco.a.cerna@gmail.com>
2013-11-11 13:01:18 -0800

    Changed javascript to coffeescript

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,15 @@ class SessionsController < ApplicationController
 
     @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     if @authorization
-      render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
+      # render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
     else
       user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
       user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       user.save
 
-      render :text => "Hi #{user.name}! You've signed up."
+      # render :text => "Hi #{user.name}! You've signed up."
     end
+    redirect_to potholes_path
   end
 
   def failure
