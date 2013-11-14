d7e2af3ca0ea716552602cdc2ba177b7b8cc75d5
Marco <marco.a.cerna@gmail.com>
2013-11-14 09:16:26 -0800

    Syncing up css and navbar changes

--- app/assets/javascripts/jquery.mouse.js
+++ app/assets/javascripts/jquery.mouse.js
@@ -3,20 +3,20 @@ $(document).ready(function(){
   var isClicked = false
 
     //When clicked once opens div, clicked again closes the div
-    $('body').on('touchstart', 'li', function(event){
+    $('body').on('touchstart', '#loginButton', function(event){
       if(isClicked ==false){
-        $(this).stop().animate({height:'100px'},{easing: 'easeOutBounce'})
+        $('.blueOne').stop().animate({height:'120px'},{easing: 'easeOutBounce'})
         isClicked=true
       }
       else{
-        $(this).stop().animate({height:'31px'},{easing: 'easeOutBounce'})
+        $('.blueOne').stop().animate({height:'31px'},{easing: 'easeOutBounce'})
         isClicked = false
       }
     });
 
     $('body').on('click', 'li', function(event){
       if(isClicked ==false){
-        $(this).stop().animate({height:'100px'},{easing: 'easeOutBounce'})
+        $(this).stop().animate({height:'120px'},{easing: 'easeOutBounce'})
         isClicked=true
       }
       else{
@@ -24,5 +24,8 @@ $(document).ready(function(){
         isClicked = false
       }
     });
-});
 
+    $('.linkText').hover(function(event){
+      //alert('hover is working')//set up toggle class here to show button is being hovered over
+    })
+});
